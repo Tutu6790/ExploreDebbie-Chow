33bf5177665f420f3c37063b7edc680f423086c7
Tutu6790 <okonudeborah28@gmail.com>
2022-07-17 18:25:04 +0100

    website first commit

---
+++ main.js
@@ -0,0 +1,17 @@
+var mybutton = document.getElementById("myBtn");
+window.onscroll = function() {
+    scrollFunction()
+};
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    }
+    else {
+        mybutton.style.display = "none";
+    }
+}
+function topFunction() {
+    window.scroll(0,0);
+}
+window.scroll(0,0);
+
